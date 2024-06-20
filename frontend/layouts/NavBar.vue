66fb344503f85bf556199d2d26ffab4fa1549b56
Teodora Kanjevac <kanjevac03@gmail.com>
2024-06-20 20:20:53 +0200

    Style Updates
    Added a NavLink Component and made some changes to navbar style

--- frontend/layouts/NavBar.vue
+++ frontend/layouts/NavBar.vue
@@ -3,12 +3,12 @@
         <nav class="bg-white border-gray-200">
             <div class="md:max-w-screen-2xl flex flex-wrap items-center justify-between md:justify-around mx-auto p-4">
                 <NuxtLink to="/" class="flex items-center">
-                    <Logo class="w-14 md:w-16" />
-                    <span class="self-center text-lg md:text-xl font-semibold pl-5">
+                    <Logo class="w-14 md:xl:w-16" />
+                    <span class="self-center text-lg md:xl:text-xl font-semibold pl-5">
                         nextcomputers.rs
                     </span>
                 </NuxtLink>
-                <div class="flex md:flex-grow md:w-40 md:xl:px-48 md:px-14 md:order-1">
+                <div class="flex md:flex-grow md:w-40 md:xl:px-36 md:pl-10 md:order-1">
                     <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search"
                         aria-expanded="false"
                         class="md:hidden text-gray-500 hover:bg-gray-100 rounded-lg text-sm px-2 me-1">
@@ -28,18 +28,26 @@
                         <SearchBar></SearchBar>
                     </div>
                     <ul
-                        class="flex flex-col p-4 md:p-0 mt-4 font-medium text-xl border border-gray-100 bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white">
+                        class="flex flex-col md:items-center p-4 md:p-0 mt-4 font-medium text-base md:xl:text-lg border border-gray-100 bg-gray-50 md:xl:space-x-8 md:space-x-5 md:flex-row md:mt-0 md:border-0 md:bg-white">
                         <li>
-                            <NuxtLink to="/"
-                                class="block py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0">
-                                About
-                            </NuxtLink>
+                            <NavLink :path="'/'" :title="'Naslovna'"
+                                class="block md:hidden py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0">
+                            </NavLink>
                         </li>
                         <li>
-                            <NuxtLink to="/"
+                            <NavLink :path="'/'" :title="'O nama'"
                                 class="block py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0">
-                                Services
-                            </NuxtLink>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink :path="'/'" :title="'Proizvodi'"
+                                class="block py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0">
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink :path="'/'" :title="'Prijavite se'"
+                                class="block py-2 px-3 md:py-1 md:px-4 text-gray-100 md:rounded-full rounded md:text-slate-50 active:bg-primary bg-primary-light">
+                            </NavLink>
                         </li>
                     </ul>
                 </div>
@@ -53,4 +61,5 @@ import SearchIcon from "~/components/icons/SearchIcon.vue"
 import HamburgerIcon from "~/components/icons/HamburgerIcon.vue"
 import SearchBar from "~/components/SearchBar.vue"
 import Logo from "~/components/Logo.vue"
+import NavLink from "~/components/NavLink.vue"
 </script>
