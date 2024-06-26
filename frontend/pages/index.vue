8f70d3bd4aa8c4f09a3528cf52c50dd68b45bec9
Teodora Kanjevac <kanjevac03@gmail.com>
2024-06-26 19:04:17 +0200

    Update index.vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <NavBar></NavBar>
+    <Hero></Hero>
     <Footer></Footer>
   </div>
 </template>
 
 <script setup>
 import NavBar from "~/layouts/NavBar.vue";
+import Hero from "~/layouts/Hero.vue";
 import Footer from "~/layouts/Footer.vue";
 </script>
 
