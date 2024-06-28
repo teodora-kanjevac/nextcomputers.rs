a273ef23daa793c68ef13476a943a8a49f2c77d5
Teodora Kanjevac <kanjevac03@gmail.com>
2024-06-28 02:47:30 +0200

    Added a Product Showcase Slider
    Added a slider with 3 different products that will be showcased

---
+++ frontend/components/ProductGrid.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div class="mb-4 grid gap-6 sm:grid-cols-2 md:mb-8 lg:grid-cols-3">
+            <Product></Product>
+            <Product></Product>
+            <Product></Product>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
