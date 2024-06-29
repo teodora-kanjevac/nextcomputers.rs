f91d8d8223b00ffdbf08d2e6f2031dc6be9c2e90
Teodora Kanjevac <kanjevac03@gmail.com>
2024-06-29 15:57:33 +0200

    Changed the arrow icons
    Changed the icons for button indicatiors and converted them to components

---
+++ frontend/components/icons/IndicatorLeftIcon.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5"
+                d="m14 7l-5 5m0 0l5 5" />
+        </svg>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'IndicatorLeftIcon'
+}
+</script>
--- frontend/layouts/ProductShowcase.vue
+++ frontend/layouts/ProductShowcase.vue
@@ -22,24 +22,18 @@
             </div>
             <div class="flex justify-center items-center pt-4">
                 <button type="button"
-                    class="flex justify-center items-center me-6 h-full cursor-pointer group focus:outline-none"
+                    class="flex justify-center items-center me-8 h-full cursor-pointer group focus:outline-none"
                     data-carousel-prev>
-                    <span class="bg-primary-light p-1 rounded-full text-white hover:bg-primary dark:hover:text-white">
-                        <svg class="size-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                            <path fill="currentColor"
-                                d="m7.825 12l3.875 3.9q.275.275.288.688t-.288.712q-.275.275-.7.275t-.7-.275l-4.6-4.6q-.15-.15-.213-.325T5.426 12t.063-.375t.212-.325l4.6-4.6q.275-.275.688-.287t.712.287q.275.275.275.7t-.275.7zm6.6 0l3.875 3.9q.275.275.288.688t-.288.712q-.275.275-.7.275t-.7-.275l-4.6-4.6q-.15-.15-.213-.325T12.026 12t.063-.375t.212-.325l4.6-4.6q.275-.275.688-.287t.712.287q.275.275.275.7t-.275.7z" />
-                        </svg>
+                    <span class="bg-primary-light p-1 rounded-full text-white hover:bg-primary">
+                        <IndicatorLeftIcon class="size-7"></IndicatorLeftIcon>
                         <span class="sr-only">Previous</span>
                     </span>
                 </button>
                 <button type="button"
-                    class="flex justify-center items-center ms-6 h-full cursor-pointer group focus:outline-none"
+                    class="flex justify-center items-center ms-8 h-full cursor-pointer group focus:outline-none"
                     data-carousel-next>
-                    <span class="bg-primary-light p-1 rounded-full text-white hover:bg-primary dark:hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="size-7" viewBox="0 0 24 24">
-                            <path fill="currentColor"
-                                d="M9.575 12L5.7 8.1q-.275-.275-.288-.687T5.7 6.7q.275-.275.7-.275t.7.275l4.6 4.6q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.6 4.6q-.275.275-.687.288T5.7 17.3q-.275-.275-.275-.7t.275-.7zm6.6 0L12.3 8.1q-.275-.275-.288-.687T12.3 6.7q.275-.275.7-.275t.7.275l4.6 4.6q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.6 4.6q-.275.275-.687.288T12.3 17.3q-.275-.275-.275-.7t.275-.7z" />
-                        </svg>
+                    <span class="bg-primary-light p-1 rounded-full text-white hover:bg-primary">
+                        <IndicatorRightIcon class="size-7"></IndicatorRightIcon>
                         <span class="sr-only">Next</span>
                     </span>
                 </button>
@@ -49,6 +43,8 @@
 </template>
 
 <script setup>
+import IndicatorLeftIcon from '~/components/icons/IndicatorLeftIcon.vue';
+import IndicatorRightIcon from '~/components/icons/IndicatorRightIcon.vue';
 </script>
 
 <style scoped>
