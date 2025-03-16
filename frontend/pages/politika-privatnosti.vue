0576a10419bad85abb729268be2c7830864b4f8c
Teodora Kanjevac <kanjevac03@gmail.com>
2025-03-16 23:01:19 +0100

    Added Privacy Policy page

---
+++ frontend/pages/politika-privatnosti.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <NavBar />
+        <PrivacyPolicy />
+        <Footer />
+    </div>
+</template>
+
+<script setup>
+import NavBar from '~/layouts/NavBar.vue'
+import Footer from '~/layouts/Footer.vue'
+import PrivacyPolicy from '~/layouts/PrivacyPolicy.vue'
+import { usePageTitle } from '~/composables/useTitle'
+
+const { updateTitle } = usePageTitle()
+updateTitle('Politika privatnosti')
+</script>
