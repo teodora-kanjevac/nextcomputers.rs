bb82d1bb829bb258813455b5b7e77cc2244c9029
Teodora Kanjevac <kanjevac03@gmail.com>
2025-03-16 23:02:24 +0100

    Added an FAQ page

---
+++ frontend/pages/faq.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <NavBar />
+        <FAQ />
+        <Footer />
+    </div>
+</template>
+
+<script setup>
+import NavBar from '~/layouts/NavBar.vue'
+import Footer from '~/layouts/Footer.vue'
+import FAQ from '~/layouts/FAQ.vue'
+import { usePageTitle } from '~/composables/useTitle'
+
+const { updateTitle } = usePageTitle()
+updateTitle('Često postavljena pitanja')
+</script>
