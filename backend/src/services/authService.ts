39cc00bc20d685ecac4912a011eaf278263d253f
Bager02 <mihajlo2002@hotmail.rs>
2025-04-06 22:23:40 +0200

    checking email uniqueness

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -7,19 +7,27 @@ export const registerUser = async (userData: RegisterUserDTO) => {
     const hashedPassword = await bcryptjs.hash(userData.password, 10);
 
     try {
-        const user = await prisma.user.create({
-            data: {
-                email: userData.email,
-                first_name: userData.firstName,
-                last_name: userData.lastName,
-                address: userData.address,
-                phone_number: userData.phoneNumber,
-                password_hash: hashedPassword,
-            },
+        const existingUser = await prisma.user.findUnique({
+          where: { email: userData.email },
         })
-
+    
+        if (existingUser) {
+          throw new Error('Email already in use')
+        }
+    
+        const user = await prisma.user.create({
+          data: {
+            email: userData.email,
+            first_name: userData.firstName,
+            last_name: userData.lastName,
+            address: userData.address,
+            phone_number: userData.phoneNumber,
+            password_hash: hashedPassword,
+          },
+        })
+    
         return new User(user)
-    } catch (error) {
+      } catch (error) {
         throw new Error('Error creating user')
-    }
+      }
 }
